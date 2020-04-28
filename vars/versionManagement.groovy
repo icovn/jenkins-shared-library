2d10b8de25389b7004b4782deb069cc53f398821
Nguyễn Quang Huy <quanghuy.ico@gmail.com>
2020-04-28 14:45:33 +0700

    xxx

--- vars/versionManagement.groovy
+++ vars/versionManagement.groovy
@@ -2,3 +2,8 @@ def call(Map config=[:]) {
     def response = httpRequest 'https://google.com.vn'
     return response
 }
+
+def test1(url) {
+    def response = httpRequest url
+    return response
+}
