7f87e1b40ac12482a8a2d7074dc6e32b8c8743ab
Nguyễn Quang Huy <quanghuy.ico@gmail.com>
2020-04-28 12:02:40 +0700

    try again

--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -1,19 +1,13 @@
 def call(Map config = [:], Closure body) {
     node {
-        git url: "https://github.com/werne2j/sample-nodejs"
+        git url: config.url
 
         stage("Install") {
-            sh "npm install"
+            sh "yarn install"
         }
 
         stage("Test") {
-            sh "npm test"
-        }
-
-        stage("Deploy") {
-            if (config.deploy) {
-                sh "npm publish"
-            }
+            sh "yarn build"
         }
 
         body()
