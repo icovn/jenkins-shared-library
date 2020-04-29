89a6985c37e12ac909ca4843c2fddf70da849a58
Nguyễn Quang Huy <quanghuy.ico@gmail.com>
2020-04-29 08:25:37 +0700

    again

--- vars/buildJavaWithSubModules.groovy
+++ vars/buildJavaWithSubModules.groovy
@@ -4,28 +4,21 @@
  * @return
  */
 def call(Map pipelineParams) {
-    stages {
-        stage('checkout git') {
-            steps {
-                git branch: pipelineParams.gitBranch, credentialsId: pipelineParams.gitCredentialsId, url: pipelineParams.gitUrl
-            }
-        }
-
-        stage('build') {
-            steps {
-                sh 'mvn clean package -DskipTests=true'
-            }
-        }
-
-        stage('build child modules') {
-            steps {
-                buildJavaSubModule modules: pipelineParams.childModules
-            }
+    stage('checkout git') {
+        steps {
+            git branch: pipelineParams.gitBranch, credentialsId: pipelineParams.gitCredentialsId, url: pipelineParams.gitUrl
         }
     }
-    post {
-        failure {
-            notifyDiscord title: "Pipeline failed", description: "git ${pipelineParams.gitUrl}"
+
+    stage('build') {
+        steps {
+            sh 'mvn clean package -DskipTests=true'
+        }
+    }
+
+    stage('build child modules') {
+        steps {
+            buildJavaSubModule modules: pipelineParams.childModules
         }
     }
 }
